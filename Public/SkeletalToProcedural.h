ab11bead82e867ad310668b3a9c407e4fd99d2b9
NickCh1996 <foouli@hotmail.com>
2022-07-30 13:35:51 +0300

    Update SkeletalToProcedural.h

--- Public/SkeletalToProcedural.h
+++ Public/SkeletalToProcedural.h
@@ -1,32 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Nick Chalvatzakis
+
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
-#include "ProceduralMeshComponent.h"
-#include "Rendering/SkeletalMeshRenderData.h"
-#include "Rendering/StaticMeshVertexBuffer.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Rendering/SkeletalMeshLODRenderData.h"
-#include "VectorTypes.h"
-#include "AnyProject.generated.h"
+
+#include "MySkeletalMeshComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class PROJECTVRODUS_API USkeletalToProcedural : public UActorComponent
+UCLASS()
+class ANYPROJECT_API UMySkeletalMeshComponent : public USkeletalMeshComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	USkeletalToProcedural();
-
-
-public:	
-
-	UFUNCTION(BlueprintCallable)
-	void SkeletalMeshToProcedural(USkeletalMeshComponent* SkeletalMeshComponent, int32 LODIndex, UProceduralMeshComponent* ProceduralMeshComponent, bool bCreateCollision);
-
-		
+		void SliceSkeletalMeshComponent(int32 LODIndex, FVector PlanePosition, FVector PlaneNormal, bool bCreateOtherHalf, USkeletalMeshComponent*& OutOtherHalfSkelMesh, UMaterialInterface* CapMaterial);
+	
 };
+
